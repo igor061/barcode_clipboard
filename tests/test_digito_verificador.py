e47501bd1d037e5a9bf60f977f235f223de0e331
igor061@gmail.com <igor061@gmail.com>
2023-03-09 10:07:18 -0300

    Teste Inicial

---
+++ tests/test_digito_verificador.py
@@ -0,0 +1,8 @@
+import ./digito_verificador
+
+
+def test_calcular_modulo11():
+    assert digito_verificador.calcular_modulo11('1234567890') == '1'
+    assert digito_verificador.calcular_modulo11('001905009') == '5'
+    assert digito_verificador.calcular_modulo11('237900000002812326800000034400005018000535') == '3'
+    assert digito_verificador.calcular_modulo11('34191747800000058771005542640000095805301400011') == '7'
