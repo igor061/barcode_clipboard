86cf48829b29b9eecb1b323d5818fadad633e598
igor061@gmail.com <igor061@gmail.com>
2023-03-09 10:56:37 -0300

    test digito_verificador

--- tests/test_digito_verificador.py
+++ tests/test_digito_verificador.py
@@ -1,8 +1,19 @@
-import digito_verificador
+from unittest import TestCase
+
+from digito_verificador import calcular_modulo11, calcular_modulo10
 
 
-def test_calcular_modulo11():
-    assert digito_verificador.calcular_modulo11('1234567890') == '1'
-    assert digito_verificador.calcular_modulo11('001905009') == '5'
-    assert digito_verificador.calcular_modulo11('237900000002812326800000034400005018000535') == '3'
-    assert digito_verificador.calcular_modulo11('34191747800000058771005542640000095805301400011') == '7'
+class TestCalcularModulor10(TestCase):
+    def test_calcular_modulo10(self):
+        assert calcular_modulo10('033990117') == '5'
+        assert calcular_modulo10('0001425680') == '4'
+        assert calcular_modulo10('0000030971') == '6'
+        assert calcular_modulo10('9434517811') == '8'
+
+    def test_calcular_modulo11(self):
+        assert calcular_modulo11('0339930200001364009011772500000000004630101') == '2'
+        assert calcular_modulo11('3649000000000099900000100014256800000030971') == '4'
+        assert calcular_modulo11('84850000004') == '8'
+        assert calcular_modulo11('06500162202') == '6'
+        assert calcular_modulo11('30309112611') == '1'
+        assert calcular_modulo11('87705916133') == '6'
